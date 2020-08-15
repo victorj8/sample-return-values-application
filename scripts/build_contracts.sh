bbfd448c36127145c3e9f7deec72267bb0f91cc9
Bradley Hart <bradley.hart-ext@block.one>
2020-08-14 21:00:14 -0400

    Adding producer_api_plugin after troubleshooting

--- scripts/build_contracts.sh
+++ scripts/build_contracts.sh
@@ -8,13 +8,10 @@ cmake -DCMAKE_FRAMEWORK_PATH=/home/gitpod/eosio.cdt/build ..
 
 make -j8
 
-sleep 1s
 cleos wallet unlock --password $(cat /password) || true
-setabi eosio ./build/contracts/boot/boot.abi
-setcode eosio ./build/contracts/boot/boot.wasm
-sleep 2s
+cleos set abi eosio ./build/contracts/boot/boot.abi -p eosio@active
+cleos set code eosio ./build/contracts/boot/boot.wasm -p eosio@active
 cleos push action eosio boot "[]" -p eosio@active
 
-sleep 1s
 cleos set abi eosio ./build/contracts/sum/sum.abi -p eosio@active
 cleos set code eosio ./build/contracts/sum/sum.wasm -p eosio@active
