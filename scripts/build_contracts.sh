cfda0afd7e174e18d6e171bd26a994650e6e8611
Bradley Hart <bradley.hart-ext@block.one>
2020-08-15 00:48:43 -0400

    Slight changes

--- scripts/build_contracts.sh
+++ scripts/build_contracts.sh
@@ -7,13 +7,13 @@ cmake -DCMAKE_FRAMEWORK_PATH=/home/gitpod/eosio.cdt/build ..
 make -j8
 
 cleos wallet unlock --password $(cat /password) || true
-cleos set abi eosio ./build/contracts/boot/boot.abi -p eosio@active
-cleos set code eosio ./build/contracts/boot/boot.wasm -p eosio@active
+cleos set abi eosio ./contracts/boot/boot.abi -p eosio@active
+cleos set code eosio ./contracts/boot/boot.wasm -p eosio@active
 cleos push action eosio boot "[]" -p eosio@active
 
-cleos set code eosio ./build/contracts/system/system.wasm -p eosio@active
-cleos set abi eosio ./build/contracts/system/system.abi -p eosio@active
+cleos set code eosio ./contracts/system/system.wasm -p eosio@active
+cleos set abi eosio ./contracts/system/system.abi -p eosio@active
 cleos push action eosio init "[]" -p eosio@active
 
-cleos set abi eosio ./build/contracts/action_results/action_results.abi -p eosio@active
-cleos set code eosio ./build/contracts/action_results/action_results.wasm -p eosio@active
+cleos set abi eosio ./contracts/action_results/action_results.abi -p eosio@active
+cleos set code eosio ./contracts/action_results/action_results.wasm -p eosio@active
